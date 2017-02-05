387f951812eda4b63107d794591cdaf273de1c3b
shupande <shupande@vip.qq.com>
2017-02-05 14:49:46 +0800

    sp
    update

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,10 @@ body{
 	margin: 0px;
 	font-family: 微软雅黑;
 }
+#app{
+  position: absolute;
+  top:0px;
+  bottom: 0px;
+  width: 100px;
+}
 </style>
--- src/components/layout.vue
+++ src/components/layout.vue
@@ -2,45 +2,35 @@
     <el-row class="tac">
         <div class="p-layout-topbar clearfix">
             <div class="p-layout-name">
-                <span class="full"><i class="fa fa-windows fa-2x" aria-hidden="true"></i>联鑫创展客户管理系统<small>&nbsp by shupan</small></span>
+                <span class="full"><i class="fa fa-windows fa-2x" aria-hidden="true"></i>联鑫创展客户管理<small>by shupan</small></span>
             </div>
-            <!-- <div class="p-layout-collapse"><i class="fa fa-bars"></i></div> -->
-            <!-- <div class="p-layout-nav">
-        <el-dropdown class="is-user">
-          <span class="el-dropdown-link">
-            admin <i class="el-icon-caret-bottom el-icon--right"></i>
-          </span>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item command="logout">退出</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-      </div> -->
+            <div class="p-layout-nav">
+              <el-dropdown class="is-user">
+              <span class="el-dropdown-link">
+                admin <i class="el-icon-caret-bottom el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="logout">退出</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
         </div>
-
-        <!-- 地址展示 -->
-        <!-- <div class="p-layout-panel">
-        <div class="p-layout-content">
-          <div class="p-layout-container">
-            <div class="p-layout-breadcrumb">
-              <el-breadcrumb separator="/">
-                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-              </el-breadcrumb>
-            </div>
+          <el-col :span="24" class="panel-center">
+          <aside class="p-layout-sider">
+            <el-menu  default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+              <el-menu-item-group>
+                  <el-menu-item :index="menu.name" v-for="menu in menus">
+                      <i  v-if="menu.icon"  class="fa" v-bind:class="'fa fa-'+menu.icon" aria-hidden="true">  
+                      </i>
+                        {{menu.text}}
+                  </el-menu-item>
+              </el-menu-item-group>
+            </el-menu>
+            </aside>
+          </el-col>
             <div class="p-layout-inner">
               <slot></slot>
             </div>
-          </div>
-        </div>
-      </div> -->
-      <el-col :span="4">
-        <el-menu mode="vertical" default-active="1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-           <el-menu-item-group title="分组一" v-for="menu in menus">
-              <el-menu-item index="menu"><i  v-if="menu.icon"  class="fa" v-bind:class="'fa fa-'+menu.icon" aria-hidden="true"></i>{{menu.text}}</el-menu-item>
-           </el-menu-item-group>
-        </el-menu>
-      </el-col>
-        <slot></slot>
       </el-row>
 </template>
 <script>
@@ -66,35 +56,54 @@ export default {
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="less" scoped>
+@mg:20px;
+
 .p-layout-topbar {
-    /*position: fixed;*/
+    /*display: flex;
+    justify-content: space-between;*/
+    position:fixed;
     width: 100%;
     height: 70px;
     line-height: 70px;
     background-color: #03a9f4;
     z-index: 101;
     color: #fff;
+    .el-dropdown-link{
+      color: #fff;
+      cursor:pointer;
+    }
 }
-.full {
-    margin-left: 20px;
-    font-weight: bold;
+.panel-center{
+  position:absolute;
+  width:100px;
+  top:70px;
+  bottom:0px;
+  z-index:100;
 }
-.p-layout-panel,.p-layout-content {
-   /* position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    right: 0;*/
-    overflow: hidden;
-    background: #f5f5f5;
-    transition: all 0.3s ease;
-    width: auto;
+i{
+  margin-right:5px;
+  
+}
+.p-layout-name{
+  float:left;
+  font-weight: bold;
+  padding-left: @mg;
+  small{
+    margin-left:5px;
   }
-.p-layout-inner {
-    padding: 10px;
-    background: #fff;
-    border-radius: 3px;
-    margin-top: 10px;
-  }
+}
+ .p-layout-nav{
+  float:right;
+  padding-right:20px;
+ }
+ .p-layout-sider{
+  position:fixed;
+  background-color:#eef1f6;
+  top:70px;
+  width:230px;
+  height:100%;
+  overflow-x:hidden;
+  
+ }
 </style>
